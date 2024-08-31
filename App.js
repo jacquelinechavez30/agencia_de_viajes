e0c1f6ed29e599a67d1fb1ab672a05ccdd6a6f19
chavez823 <notaschavez@gmail.com>
2024-08-31 10:46:55 -0600

    modal

--- App.js
+++ App.js
@@ -46,13 +46,33 @@ const styles = StyleSheet.create({
 
 export default App;*/
 //Parte 2
-import React from 'react';
-import {View, StyleSheet, Image, Text, ScrollView} from 'react-native';
+import React, { useState } from 'react';
+import {View, StyleSheet, Image, Text, ScrollView, Modal, Button, TouchableHighlight} from 'react-native';
 
 const App = () =>{
+  const {modalVisibleplaya, setModalVisibleplaya}= useState(false);
   return(
     <>
     <ScrollView>
+    <Modal
+  transparent={true}
+  animationType="slide"
+  visible={modalVisibleplaya}
+  onRequestClose={() => {
+    alert('Modal has been closed.');
+  }}
+>
+  <View style={styles.vistaModal}>
+    <View style={styles.Modal}>
+      <Text style={styles.subtitulo}>Ir a la playa</Text>
+      <Text>El Salvador cuenta con hermosas playas a nivel Centroamérica.</Text>
+      <Button
+        title="Cerrar"
+        onPress={() => setModalVisibleplaya(!modalVisibleplaya)}
+      />
+    </View>
+  </View>
+</Modal>
     <View style={{ flexDirection: 'row' }}>
   <Image style={styles.banner} source={require('./src/img/bg.jpg')} />
   </View>
@@ -61,7 +81,14 @@ const App = () =>{
     <Text style={styles.titulo}>Que hacer en El Salvador</Text>
     <ScrollView horizontal>
       <View>
-        <Image style={styles.ciudad} source={require('./src/img/actividad1.jpg')} />
+      <TouchableHighlight
+  onPress={() => setModalVisibleplaya(!modalVisibleplaya)}
+>
+  <Image
+    style={styles.ciudad}
+    source={require('./src/img/actividad1.jpg')}
+  />
+</TouchableHighlight>
       </View>
       <View>
         <Image style={styles.ciudad} source={require('./src/img/actividad2.jpg')} />
@@ -76,7 +103,8 @@ const App = () =>{
         <Image style={styles.ciudad} source={require('./src/img/actividad5.jpg')} />
       </View>
     </ScrollView>
-    <Text style={styles.titulo}>Platillos Salvadoreños</Text>
+  </View>
+  <Text style={styles.titulo}>Platillos Salvadoreños</Text>
 <View>
   <View>
     <Image style={styles.mejores} source={require('./src/img/mejores1.jpg')} />
@@ -103,11 +131,6 @@ const App = () =>{
     <Image style={styles.mejores} source={require('./src/img/ruta4.jpg')} />
   </View>
 </View>
-  </View>
-
-
-  
-
 </>
   );
 };
@@ -141,7 +164,25 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     flexWrap: 'wrap',
     justifyContent: 'space-between'
-  }
+  },
+  vistaModal: {
+    backgroundColor: '#000000aa', 
+    flex: 1,
+  },
+  
+  Modal: {
+    backgroundColor: '#fff', 
+    margin: 50,
+    padding: 40, 
+    borderRadius: 10, 
+    flex: 1, 
+  },
+  
+  subtitulo: {
+    fontWeight: 'bold', 
+    fontSize: 14, 
+    justifyContent: 'center', 
+  },
   
 });
 
