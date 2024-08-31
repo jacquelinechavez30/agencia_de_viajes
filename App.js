6b09c1392d53e63b58e254ff3e92e74b23134f4e
chavez823 <notaschavez@gmail.com>
2024-08-31 10:24:03 -0600

    Rutas turisticas

--- App.js
+++ App.js
@@ -1,20 +1,148 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/*import React from 'react';
+import {View, StyleSheet} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+const App = () =>{
+  return(
+    <>
+<View style={styles.contenedor}>
+  <View style={styles.caja1}></View>
+  <View style={styles.caja2}></View>
+  <View style={styles.caja3}></View>
+  <View style={styles.caja4}></View>
+</View>
+</>
   );
-}
-
+};
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  contenedor:{
+    backgroundColor:'cornflowerblue',
+    flex:1,
+    flexDirection:'row',
+    justifyContent: 'space-around',
+    alignItems:'flex-end'
   },
+  caja1:{
+    padding:20,
+    flex:1,
+    backgroundColor:'navy'
+  },
+  caja2:{
+    padding:20,
+    flex:1,
+    backgroundColor:'yellow'
+  },
+  caja3:{
+    padding:20,
+    flex:1,
+    backgroundColor:'green'
+  },
+  caja4:{
+    padding:20,
+    flex:1,
+    backgroundColor:'teal'
+  },
+
 });
+
+export default App;*/
+//Parte 2
+import React from 'react';
+import {View, StyleSheet, Image, Text, ScrollView} from 'react-native';
+
+const App = () =>{
+  return(
+    <>
+    <ScrollView>
+    <View style={{ flexDirection: 'row' }}>
+  <Image style={styles.banner} source={require('./src/img/bg.jpg')} />
+  </View>
+  </ScrollView> 
+  <View style={styles.contenedor}>
+    <Text style={styles.titulo}>Que hacer en El Salvador</Text>
+    <ScrollView horizontal>
+      <View>
+        <Image style={styles.ciudad} source={require('./src/img/actividad1.jpg')} />
+      </View>
+      <View>
+        <Image style={styles.ciudad} source={require('./src/img/actividad2.jpg')} />
+      </View>
+      <View>
+        <Image style={styles.ciudad} source={require('./src/img/actividad3.jpg')} />
+      </View>
+      <View>
+        <Image style={styles.ciudad} source={require('./src/img/actividad4.jpg')} />
+      </View>
+      <View>
+        <Image style={styles.ciudad} source={require('./src/img/actividad5.jpg')} />
+      </View>
+    </ScrollView>
+    <Text style={styles.titulo}>Platillos Salvadoreños</Text>
+<View>
+  <View>
+    <Image style={styles.mejores} source={require('./src/img/mejores1.jpg')} />
+  </View>
+  <View>
+    <Image style={styles.mejores} source={require('./src/img/mejores2.jpg')} />
+  </View>
+  <View>
+    <Image style={styles.mejores} source={require('./src/img/mejores3.jpg')} />
+  </View>
+</View>
+<Text style={styles.titulo}>Rutas Turísticas</Text>
+<View style={styles.listado}>
+  <View style={styles.listaItem}>
+    <Image style={styles.mejores} source={require('./src/img/ruta1.jpg')} />
+  </View>
+  <View style={styles.listaItem}>
+    <Image style={styles.mejores} source={require('./src/img/ruta2.jpg')} />
+  </View>
+  <View style={styles.listaItem}>
+    <Image style={styles.mejores} source={require('./src/img/ruta3.jpg')} />
+  </View>
+  <View style={styles.listaItem}>
+    <Image style={styles.mejores} source={require('./src/img/ruta4.jpg')} />
+  </View>
+</View>
+  </View>
+
+
+  
+
+</>
+  );
+};
+const styles = StyleSheet.create({
+  banner: {
+    height: 250,
+    flex: 1
+  },
+  titulo: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    marginVertical: 10
+  },
+  contenedor: {
+    marginHorizontal: 10
+  },
+  ciudad: {
+    width: 250,
+    height: 300,
+    marginRight: 10
+  },
+  mejores: {
+    width: '100%',
+    height: 200,
+    marginVertical: 5
+  },
+  listaItem:{
+    flexBasis: '49%'
+  },
+  listado: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between'
+  }
+  
+});
+
+export default App;
